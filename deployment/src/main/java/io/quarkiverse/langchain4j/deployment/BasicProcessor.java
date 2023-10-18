c1e137ac1e22468a3818952b86f6997f13461733
Georgios Andrianakis <geoand@gmail.com>
2023-10-18 12:34:38 +0300

    Split up Langchain4jProcessor

--- deployment/src/main/java/io/quarkiverse/langchain4j/deployment/BasicProcessor.java
+++ deployment/src/main/java/io/quarkiverse/langchain4j/deployment/BasicProcessor.java
@@ -2,50 +2,33 @@ package io.quarkiverse.langchain4j.deployment;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.lang.reflect.Modifier;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.function.BiFunction;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
-import org.jboss.jandex.AnnotationInstance;
-import org.jboss.jandex.AnnotationTarget;
-import org.jboss.jandex.AnnotationValue;
-import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.DotName;
-import org.jboss.jandex.FieldInfo;
-import org.jboss.jandex.IndexView;
-import org.objectweb.asm.ClassVisitor;
 
 import com.knuddels.jtokkit.Encodings;
 
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.chat.StreamingChatLanguageModel;
 import dev.langchain4j.model.embedding.EmbeddingModel;
-import dev.langchain4j.model.input.structured.StructuredPrompt;
-import dev.langchain4j.model.input.structured.StructuredPromptProcessor;
 import dev.langchain4j.model.language.LanguageModel;
 import dev.langchain4j.model.language.StreamingLanguageModel;
 import dev.langchain4j.model.moderation.ModerationModel;
-import io.quarkiverse.langchain4j.QuarkusPromptTemplateFactory;
 import io.quarkiverse.langchain4j.runtime.Langchain4jModelsRecorder;
-import io.quarkiverse.langchain4j.runtime.StructuredPromptsRecorder;
 import io.quarkiverse.langchain4j.runtime.config.LangChain4jBuildConfig;
 import io.quarkiverse.langchain4j.runtime.config.LangChain4jRuntimeConfig;
 import io.quarkiverse.langchain4j.runtime.config.ModelProvider;
-import io.quarkiverse.langchain4j.runtime.prompt.Mappable;
 import io.quarkus.arc.deployment.BeanDiscoveryFinishedBuildItem;
 import io.quarkus.arc.deployment.SyntheticBeanBuildItem;
 import io.quarkus.arc.processor.InjectionPointInfo;
@@ -53,21 +36,13 @@ import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
 import io.quarkus.deployment.annotations.Record;
-import io.quarkus.deployment.builditem.BytecodeTransformerBuildItem;
-import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.IndexDependencyBuildItem;
 import io.quarkus.deployment.builditem.ShutdownContextBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBuildItem;
-import io.quarkus.gizmo.ClassTransformer;
-import io.quarkus.gizmo.MethodCreator;
-import io.quarkus.gizmo.MethodDescriptor;
-import io.quarkus.gizmo.ResultHandle;
-import io.quarkus.qute.Expression;
-import io.quarkus.qute.Template;
 import io.quarkus.runtime.configuration.ConfigurationException;
 
-class Langchain4jProcessor {
+class BasicProcessor {
 
     private static final String FEATURE = "langchain4j";
     private static final DotName CHAT_MODEL = DotName.createSimple(ChatLanguageModel.class);
@@ -76,16 +51,6 @@ class Langchain4jProcessor {
     private static final DotName STREAMING_LANGUAGE_MODEL = DotName.createSimple(StreamingLanguageModel.class);
     private static final DotName EMBEDDING_MODEL = DotName.createSimple(EmbeddingModel.class);
     private static final DotName MODERATION_MODEL = DotName.createSimple(ModerationModel.class);
-    private static final DotName STRUCTURED_PROMPT = DotName.createSimple(StructuredPrompt.class);
-    private static final DotName STRUCTURED_PROMPT_PROCESSOR = DotName.createSimple(StructuredPromptProcessor.class);
-    public static final DotName OBJECT = DotName.createSimple(Object.class.getName());
-
-    public static final MethodDescriptor MAP_PUT = MethodDescriptor.ofMethod(Map.class, "put", Object.class, Object.class,
-            Object.class);
-    public static final MethodDescriptor MAP_PUT_ALL = MethodDescriptor.ofMethod(Map.class, "putAll", void.class, Map.class);
-
-    public static final MethodDescriptor MAPPABLE_OBTAIN = MethodDescriptor.ofMethod(Mappable.class, "obtainFieldValuesMap",
-            Map.class);
 
     @BuildStep
     FeatureBuildItem feature() {
@@ -252,104 +217,4 @@ class Langchain4jProcessor {
                 configNamespace);
     }
 
-    @BuildStep
-    @Record(ExecutionTime.STATIC_INIT)
-    public void structuredPromptSupport(StructuredPromptsRecorder recorder,
-            CombinedIndexBuildItem combinedIndexBuildItem,
-            BuildProducer<BytecodeTransformerBuildItem> transformerProducer) {
-        IndexView index = combinedIndexBuildItem.getIndex();
-
-        Collection<AnnotationInstance> instances = index.getAnnotations(STRUCTURED_PROMPT);
-        for (AnnotationInstance instance : instances) {
-            AnnotationTarget target = instance.target();
-            if (target.kind() != AnnotationTarget.Kind.CLASS) {
-                continue; // should never happen
-            }
-            String[] parts = instance.value().asStringArray();
-            AnnotationValue delimiterValue = instance.value("delimiter");
-            String delimiter = delimiterValue != null ? delimiterValue.asString() : "\n";
-            String promptTemplateString = String.join(delimiter, parts);
-            ClassInfo annotatedClass = target.asClass();
-            boolean hasNestedParams = isMappable(promptTemplateString);
-            if (!hasNestedParams) {
-                ClassInfo current = annotatedClass;
-                while (true) {
-                    DotName superName = current.superName();
-                    ClassInfo superClassInfo = OBJECT.equals(superName) ? null : index.getClassByName(superName);
-                    transformerProducer.produce(new BytecodeTransformerBuildItem(current.name().toString(),
-                            new StructuredPromptAnnotatedTransformer(current, superClassInfo != null,
-                                    superName.toString())));
-                    if (superClassInfo == null) {
-                        break;
-                    }
-                    current = superClassInfo;
-                }
-
-            }
-            recorder.add(annotatedClass.name().toString(), promptTemplateString);
-        }
-    }
-
-    /**
-     * We can obtain a map of the class values if the template does not try to access values in any nested level
-     */
-    private static boolean isMappable(String promptTemplateString) {
-        Template template = QuarkusPromptTemplateFactory.ENGINE.parse(promptTemplateString);
-        boolean hasNestedParams = false;
-        List<Expression> expressions = template.getExpressions();
-        for (Expression expression : expressions) {
-            if (expression.getParts().size() > 1) {
-                hasNestedParams = true;
-            }
-        }
-        return hasNestedParams;
-    }
-
-    /**
-     * Simple class transformer that adds the {@link Mappable} interface to the class and implement the method
-     * by simply reading all properties of the class itself
-     */
-    private static class StructuredPromptAnnotatedTransformer implements BiFunction<String, ClassVisitor, ClassVisitor> {
-
-        private final ClassInfo annotatedClass;
-        private final boolean hasSuperMappable;
-        private final String superClassName;
-
-        private StructuredPromptAnnotatedTransformer(ClassInfo annotatedClass, boolean hasSuperMappable,
-                String superClassName) {
-            this.annotatedClass = annotatedClass;
-            this.hasSuperMappable = hasSuperMappable;
-            this.superClassName = superClassName;
-        }
-
-        @Override
-        public ClassVisitor apply(String s, ClassVisitor classVisitor) {
-            ClassTransformer transformer = new ClassTransformer(annotatedClass.name().toString());
-            transformer.addInterface(Mappable.class);
-
-            MethodCreator mc = transformer.addMethod("obtainFieldValuesMap", Map.class);
-            ResultHandle mapHandle = mc.newInstance(MethodDescriptor.ofConstructor(HashMap.class));
-            for (FieldInfo field : annotatedClass.fields()) {
-                short modifiers = field.flags();
-                if (Modifier.isStatic(modifiers) || Modifier.isTransient(modifiers)) {
-                    continue;
-                }
-                String name = field.name();
-                ResultHandle fieldValue = mc.readInstanceField(field, mc.getThis());
-                mc.invokeInterfaceMethod(MAP_PUT, mapHandle, mc.load(name), fieldValue);
-
-            }
-            if (hasSuperMappable) {
-                ResultHandle mapFromSuper = mc
-                        .invokeSpecialMethod(MethodDescriptor.ofMethod(superClassName, "obtainFieldValuesMap",
-                                Map.class), mc.getThis());
-                mc.invokeInterfaceMethod(MAP_PUT_ALL, mapFromSuper, mapHandle);
-                mc.returnValue(mapFromSuper);
-            } else {
-                mc.returnValue(mapHandle);
-            }
-
-            return transformer.applyTo(classVisitor);
-        }
-    }
 }
---
+++ deployment/src/main/java/io/quarkiverse/langchain4j/deployment/PromptProcessor.java
@@ -0,0 +1,146 @@
+package io.quarkiverse.langchain4j.deployment;
+
+import java.lang.reflect.Modifier;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+
+import org.jboss.jandex.AnnotationInstance;
+import org.jboss.jandex.AnnotationTarget;
+import org.jboss.jandex.AnnotationValue;
+import org.jboss.jandex.ClassInfo;
+import org.jboss.jandex.DotName;
+import org.jboss.jandex.FieldInfo;
+import org.jboss.jandex.IndexView;
+import org.objectweb.asm.ClassVisitor;
+
+import dev.langchain4j.model.input.structured.StructuredPrompt;
+import io.quarkiverse.langchain4j.QuarkusPromptTemplateFactory;
+import io.quarkiverse.langchain4j.runtime.StructuredPromptsRecorder;
+import io.quarkiverse.langchain4j.runtime.prompt.Mappable;
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.ExecutionTime;
+import io.quarkus.deployment.annotations.Record;
+import io.quarkus.deployment.builditem.BytecodeTransformerBuildItem;
+import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
+import io.quarkus.gizmo.ClassTransformer;
+import io.quarkus.gizmo.MethodCreator;
+import io.quarkus.gizmo.MethodDescriptor;
+import io.quarkus.gizmo.ResultHandle;
+import io.quarkus.qute.Expression;
+import io.quarkus.qute.Template;
+
+public class PromptProcessor {
+
+    private static final DotName STRUCTURED_PROMPT = DotName.createSimple(StructuredPrompt.class);
+    public static final DotName OBJECT = DotName.createSimple(Object.class.getName());
+
+    public static final MethodDescriptor MAP_PUT = MethodDescriptor.ofMethod(Map.class, "put", Object.class, Object.class,
+            Object.class);
+    public static final MethodDescriptor MAP_PUT_ALL = MethodDescriptor.ofMethod(Map.class, "putAll", void.class, Map.class);
+
+    @BuildStep
+    @Record(ExecutionTime.STATIC_INIT)
+    public void structuredPromptSupport(StructuredPromptsRecorder recorder,
+            CombinedIndexBuildItem combinedIndexBuildItem,
+            BuildProducer<BytecodeTransformerBuildItem> transformerProducer) {
+        IndexView index = combinedIndexBuildItem.getIndex();
+
+        Collection<AnnotationInstance> instances = index.getAnnotations(STRUCTURED_PROMPT);
+        for (AnnotationInstance instance : instances) {
+            AnnotationTarget target = instance.target();
+            if (target.kind() != AnnotationTarget.Kind.CLASS) {
+                continue; // should never happen
+            }
+            String[] parts = instance.value().asStringArray();
+            AnnotationValue delimiterValue = instance.value("delimiter");
+            String delimiter = delimiterValue != null ? delimiterValue.asString() : "\n";
+            String promptTemplateString = String.join(delimiter, parts);
+            ClassInfo annotatedClass = target.asClass();
+            boolean hasNestedParams = isMappable(promptTemplateString);
+            if (!hasNestedParams) {
+                ClassInfo current = annotatedClass;
+                while (true) {
+                    DotName superName = current.superName();
+                    ClassInfo superClassInfo = OBJECT.equals(superName) ? null : index.getClassByName(superName);
+                    transformerProducer.produce(new BytecodeTransformerBuildItem(current.name().toString(),
+                            new StructuredPromptAnnotatedTransformer(current, superClassInfo != null,
+                                    superName.toString())));
+                    if (superClassInfo == null) {
+                        break;
+                    }
+                    current = superClassInfo;
+                }
+
+            }
+            recorder.add(annotatedClass.name().toString(), promptTemplateString);
+        }
+    }
+
+    /**
+     * We can obtain a map of the class values if the template does not try to access values in any nested level
+     */
+    private static boolean isMappable(String promptTemplateString) {
+        Template template = QuarkusPromptTemplateFactory.ENGINE.parse(promptTemplateString);
+        boolean hasNestedParams = false;
+        List<Expression> expressions = template.getExpressions();
+        for (Expression expression : expressions) {
+            if (expression.getParts().size() > 1) {
+                hasNestedParams = true;
+            }
+        }
+        return hasNestedParams;
+    }
+
+    /**
+     * Simple class transformer that adds the {@link Mappable} interface to the class and implement the method
+     * by simply reading all properties of the class itself
+     */
+    private static class StructuredPromptAnnotatedTransformer implements
+            BiFunction<String, ClassVisitor, ClassVisitor> {
+
+        private final ClassInfo annotatedClass;
+        private final boolean hasSuperMappable;
+        private final String superClassName;
+
+        private StructuredPromptAnnotatedTransformer(ClassInfo annotatedClass, boolean hasSuperMappable,
+                String superClassName) {
+            this.annotatedClass = annotatedClass;
+            this.hasSuperMappable = hasSuperMappable;
+            this.superClassName = superClassName;
+        }
+
+        @Override
+        public ClassVisitor apply(String s, ClassVisitor classVisitor) {
+            ClassTransformer transformer = new ClassTransformer(annotatedClass.name().toString());
+            transformer.addInterface(Mappable.class);
+
+            MethodCreator mc = transformer.addMethod("obtainFieldValuesMap", Map.class);
+            ResultHandle mapHandle = mc.newInstance(MethodDescriptor.ofConstructor(HashMap.class));
+            for (FieldInfo field : annotatedClass.fields()) {
+                short modifiers = field.flags();
+                if (Modifier.isStatic(modifiers) || Modifier.isTransient(modifiers)) {
+                    continue;
+                }
+                String name = field.name();
+                ResultHandle fieldValue = mc.readInstanceField(field, mc.getThis());
+                mc.invokeInterfaceMethod(MAP_PUT, mapHandle, mc.load(name), fieldValue);
+
+            }
+            if (hasSuperMappable) {
+                ResultHandle mapFromSuper = mc
+                        .invokeSpecialMethod(MethodDescriptor.ofMethod(superClassName, "obtainFieldValuesMap",
+                                Map.class), mc.getThis());
+                mc.invokeInterfaceMethod(MAP_PUT_ALL, mapFromSuper, mapHandle);
+                mc.returnValue(mapFromSuper);
+            } else {
+                mc.returnValue(mapHandle);
+            }
+
+            return transformer.applyTo(classVisitor);
+        }
+    }
+}
