794febabcfc1d375f99414cb05d9cbee23e08cda
Georgios Andrianakis <geoand@gmail.com>
2023-10-24 17:12:33 +0300

    Add integration test for tools

---
+++ integration-tests/src/test/java/org/acme/example/aiservices/AssistantResourceIT.java
@@ -0,0 +1,7 @@
+package org.acme.example.aiservices;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class AssistantResourceIT extends AssistantResourceTest {
+}
