185a0409d8b7487409b3778871dfa7efb2623728
Georgios Andrianakis <geoand@gmail.com>
2023-11-06 15:31:28 +0200

    Simplify TriagedReview

--- samples/review-triage/src/main/java/io/quarkiverse/langchain4j/sample/TriagedReview.java
+++ samples/review-triage/src/main/java/io/quarkiverse/langchain4j/sample/TriagedReview.java
@@ -5,9 +5,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 public record TriagedReview(Evaluation evaluation, String message) {
 
     @JsonCreator
-    public TriagedReview(Evaluation evaluation, String message) {
-        this.evaluation = evaluation;
-        this.message = message;
+    public TriagedReview {
     }
 
 }
