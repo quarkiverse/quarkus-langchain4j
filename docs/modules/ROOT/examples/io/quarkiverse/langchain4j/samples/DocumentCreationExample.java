27387bb6a6f8433ea5a001723b258fa21776ab21
Clement Escoffier <clement.escoffier@gmail.com>
2023-11-13 09:41:20 +0100

    Document document retrievers

---
+++ docs/modules/ROOT/examples/io/quarkiverse/langchain4j/samples/DocumentCreationExample.java
@@ -0,0 +1,34 @@
+package io.quarkiverse.langchain4j.samples;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.Metadata;
+
+public class DocumentCreationExample {
+
+    Document createFromCSVLine(List<String> headers, List<String> line,
+            List<String> columnsToIncludeInMetadata) {
+        Map<String, String> metadata = new HashMap<>();
+        var content = new StringBuilder();
+        for (int i = 0; i < headers.size(); i++) {
+            var columnName = headers.get(i);
+            var value = line.get(i).trim();
+
+            if (columnName.trim().isEmpty()) {
+                continue;
+            }
+
+            if (columnsToIncludeInMetadata.contains(columnName)) {
+                metadata.put(columnName, value);
+            }
+            // We compute a Text format for the CSV line: key: value, key: value, ...
+            content.append(columnName).append(": ").append(value).append(", ");
+        }
+        // The \n is added to the end of the content
+        return new Document(content.append("\n").toString(), Metadata.from(metadata));
+    }
+
+}
