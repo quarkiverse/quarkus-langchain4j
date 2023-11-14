37f899763cb2afcf64cd8892948c3e165f4b8a95
Georgios Andrianakis <geoand@gmail.com>
2023-11-14 08:28:07 +0200

    Dummy change

--- core/runtime/src/main/java/io/quarkiverse/langchain4j/CreatedAware.java
+++ core/runtime/src/main/java/io/quarkiverse/langchain4j/CreatedAware.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 /**
  * Used to make Quarkus aware of classes being used in {@link dev.langchain4j.service.AiServices#create}
  */
-@Retention(RUNTIME)
+@Retention(RUNTIME
 @Target(ElementType.TYPE)
 public @interface CreatedAware {
 }
