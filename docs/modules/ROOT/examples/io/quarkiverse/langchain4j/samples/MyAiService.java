ccbec21a42639eac32913b9dea50d26648efbf6f
Georgios Andrianakis <geoand@gmail.com>
2023-11-22 10:20:40 +0200

    Update docs to reflect simpler `@RegisterAiService` configuration

--- docs/modules/ROOT/examples/io/quarkiverse/langchain4j/samples/MyAiService.java
+++ docs/modules/ROOT/examples/io/quarkiverse/langchain4j/samples/MyAiService.java
@@ -5,14 +5,13 @@ import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
 
 @RegisterAiService( // <1>
-        chatMemoryProviderSupplier = RegisterAiService.BeanChatMemoryProviderSupplier.class, // <2>
-        tools = EmailService.class // <3>
+        tools = EmailService.class // <2>
 )
 public interface MyAiService {
 
-    @SystemMessage("You are a professional poet") // <4>
+    @SystemMessage("You are a professional poet") // <3>
     @UserMessage("""
-                Write a poem about {topic}. The poem should be {lines} lines long. Then send this poem by email. // <5>
+                Write a poem about {topic}. The poem should be {lines} lines long. Then send this poem by email. // <4>
             """)
-    String writeAPoem(String topic, int lines); // <6>
+    String writeAPoem(String topic, int lines); // <5>
 }
