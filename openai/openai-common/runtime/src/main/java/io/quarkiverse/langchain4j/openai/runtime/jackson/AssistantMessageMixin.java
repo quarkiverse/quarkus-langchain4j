528ecc1625c36ec11c33fef03f21caf487455eaf
Georgios Andrianakis <geoand@gmail.com>
2024-01-08 13:06:09 +0200

    Fix issue with message field serialization
    Originally reported in:
    https://quarkusio.zulipchat.com/#narrow/stream/187030-users/topic/quarkus-langchain-openai.20is.20broken

--- openai/openai-common/runtime/src/main/java/io/quarkiverse/langchain4j/openai/runtime/jackson/AssistantMessageMixin.java
+++ openai/openai-common/runtime/src/main/java/io/quarkiverse/langchain4j/openai/runtime/jackson/AssistantMessageMixin.java
@@ -1,6 +1,7 @@
 package io.quarkiverse.langchain4j.openai.runtime.jackson;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import dev.ai4j.openai4j.chat.AssistantMessage;
@@ -9,6 +10,7 @@ import io.quarkus.jackson.JacksonMixin;
 @JacksonMixin(AssistantMessage.class)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
 @JsonDeserialize(builder = AssistantMessage.Builder.class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public abstract class AssistantMessageMixin {
 
 }
--- openai/openai-common/runtime/src/main/java/io/quarkiverse/langchain4j/openai/runtime/jackson/UserMessageMixin.java
+++ openai/openai-common/runtime/src/main/java/io/quarkiverse/langchain4j/openai/runtime/jackson/UserMessageMixin.java
@@ -1,6 +1,7 @@
 package io.quarkiverse.langchain4j.openai.runtime.jackson;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import dev.ai4j.openai4j.chat.UserMessage;
@@ -9,5 +10,6 @@ import io.quarkus.jackson.JacksonMixin;
 @JacksonMixin(UserMessage.class)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
 @JsonDeserialize(builder = UserMessage.Builder.class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public abstract class UserMessageMixin {
 }
