cefa3e188945e5a62db35ae282574f91c2095142
Georgios Andrianakis <geoand@gmail.com>
2024-02-16 20:42:48 +0200

    Fix formatting

--- infinispan/deployment/src/main/java/io/quarkiverse/langchain4j/infinispan/DevServicesConfigBuilderCustomizer.java
+++ infinispan/deployment/src/main/java/io/quarkiverse/langchain4j/infinispan/DevServicesConfigBuilderCustomizer.java
@@ -2,11 +2,12 @@ package io.quarkiverse.langchain4j.infinispan;
 
 import java.util.Map;
 
+import org.infinispan.commons.util.Version;
+import org.infinispan.server.test.core.InfinispanContainer;
+
 import io.smallrye.config.PropertiesConfigSource;
 import io.smallrye.config.SmallRyeConfigBuilder;
 import io.smallrye.config.SmallRyeConfigBuilderCustomizer;
-import org.infinispan.commons.util.Version;
-import org.infinispan.server.test.core.InfinispanContainer;
 
 /**
  * Ensure we set the Infinispan image name so users don't have to.
@@ -18,7 +19,9 @@ public class DevServicesConfigBuilderCustomizer implements SmallRyeConfigBuilder
     public void configBuilder(final SmallRyeConfigBuilder builder) {
         // use a priority of 50 to make sure that this is overridable by any of the standard methods
         builder.withSources(
-                new PropertiesConfigSource(Map.of("quarkus.infinispan-client.devservices.image-name", InfinispanContainer.IMAGE_BASENAME + ":" +  Version.getMajorMinor()),
+                new PropertiesConfigSource(
+                        Map.of("quarkus.infinispan-client.devservices.image-name",
+                                InfinispanContainer.IMAGE_BASENAME + ":" + Version.getMajorMinor()),
                         "quarkus-langchain4j-infinispan", 50));
     }
 }
