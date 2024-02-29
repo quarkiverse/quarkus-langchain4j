35be8f053e8a4440599e9055b202784c2d39265d
Jan Martiska <jmartisk@redhat.com>
2024-02-29 08:32:25 +0100

    Update docs with advanced RAG

---
+++ docs/modules/ROOT/examples/io/quarkiverse/langchain4j/samples/RetrievalAugmentorExample.java
@@ -0,0 +1,35 @@
+package io.quarkiverse.langchain4j.samples;
+
+import java.util.function.Supplier;
+
+import jakarta.inject.Singleton;
+
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.rag.DefaultRetrievalAugmentor;
+import dev.langchain4j.rag.RetrievalAugmentor;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import io.quarkiverse.langchain4j.redis.RedisEmbeddingStore;
+
+@Singleton
+public class RetrievalAugmentorExample implements Supplier<RetrievalAugmentor> {
+
+    private final RetrievalAugmentor augmentor;
+
+    RetrievalAugmentorExample(RedisEmbeddingStore store, EmbeddingModel model) {
+        EmbeddingStoreContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
+                .embeddingModel(model)
+                .embeddingStore(store)
+                .maxResults(3)
+                .build();
+        augmentor = DefaultRetrievalAugmentor
+                .builder()
+                .contentRetriever(contentRetriever)
+                .build();
+    }
+
+    @Override
+    public RetrievalAugmentor get() {
+        return augmentor;
+    }
+
+}
