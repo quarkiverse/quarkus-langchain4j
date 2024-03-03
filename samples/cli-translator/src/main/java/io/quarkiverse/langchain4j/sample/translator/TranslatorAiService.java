3d39cc38192ff2ba8cd9821760fadec69283dd64
Dimitris Andreadis <dimitris.andreadis@gmail.com>
2024-03-04 00:07:16 +0100

    Add cli-translator sample

---
+++ samples/cli-translator/src/main/java/io/quarkiverse/langchain4j/sample/translator/TranslatorAiService.java
@@ -0,0 +1,19 @@
+package io.quarkiverse.langchain4j.sample.translator;
+
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+import io.quarkiverse.langchain4j.RegisterAiService;
+
+@RegisterAiService
+public interface TranslatorAiService {
+
+    @SystemMessage("You are a professional translator.")
+    @UserMessage("""
+                Translate the text delimited by '---' into {language}.
+                Do not include the delimiter in the translation.
+                ---
+                {text}
+            """)
+    String translate(String text, String language);
+
+}
