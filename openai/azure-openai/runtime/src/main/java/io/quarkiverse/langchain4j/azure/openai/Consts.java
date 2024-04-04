57b7661a69aa99f8548734f172d1031da03f9866
Julien Dubois <julien.dubois@gmail.com>
2024-04-04 10:54:00 +0200

    Add a specific user agent

--- openai/azure-openai/runtime/src/main/java/io/quarkiverse/langchain4j/azure/openai/Consts.java
+++ openai/azure-openai/runtime/src/main/java/io/quarkiverse/langchain4j/azure/openai/Consts.java
@@ -2,7 +2,7 @@ package io.quarkiverse.langchain4j.azure.openai;
 
 final class Consts {
 
-    static final String DEFAULT_USER_AGENT = "langchain4j-azure-openai";
+    static final String DEFAULT_USER_AGENT = "langchain4j-quarkus-azure-openai";
 
     private Consts() {
     }
