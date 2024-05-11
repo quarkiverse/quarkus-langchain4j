632fd67e733fffa7210041c5a616be8173cbbe4a
Ioannis Canellos <iocanel@gmail.com>
2024-05-11 16:04:25 +0300

    refactor: jokes jbang bot to be picocli based

--- samples/jbang-joke-bot/jokes.java
+++ samples/jbang-joke-bot/jokes.java
@@ -1,13 +1,20 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS io.quarkus.platform:quarkus-bom:3.9.4@pom
-//DEPS io.quarkus:quarkus-vertx-http:3.9.4
+//DEPS io.quarkus:quarkus-picocli
 //DEPS io.quarkiverse.langchain4j:quarkus-langchain4j-openai:0.14.0
-
+//Q:CONFIG quarkus.banner.enabled=false
+//Q:CONFIG quarkus.log.level=WARN
 import dev.langchain4j.model.chat.ChatLanguageModel;
-import io.vertx.ext.web.Router;
-import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
+import picocli.CommandLine.Command;
 
-public class jokes {
-    void route(@Observes Router router, ChatLanguageModel ai) {
-        router.get("/joke").blockingHandler(rc -> rc.end(ai.generate("tell me a joke")));
+@Command
+public class jokes implements Runnable {
+    @Inject
+    private ChatLanguageModel ai;
+
+    @Override
+    public void run() {
+        System.out.println(ai.generate("tell me a joke"));
     }
 }
