4dd6f99efab005e8fff137b24763714976358830
Jan Martiska <jmartisk@redhat.com>
2024-06-24 09:05:46 +0200

    Use the Quarkus worker pool for in-process embedding models

--- core/runtime/src/main/java/io/quarkiverse/langchain4j/runtime/InProcessEmbeddingRecorder.java
+++ core/runtime/src/main/java/io/quarkiverse/langchain4j/runtime/InProcessEmbeddingRecorder.java
@@ -1,11 +1,13 @@
 package io.quarkiverse.langchain4j.runtime;
 
+import java.util.concurrent.Executor;
 import java.util.function.Supplier;
 
 import org.jboss.logging.Logger;
 
 import dev.langchain4j.model.embedding.AbstractInProcessEmbeddingModel;
 import io.quarkus.runtime.annotations.Recorder;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
 
 @Recorder
 public class InProcessEmbeddingRecorder {
@@ -15,9 +17,18 @@ public class InProcessEmbeddingRecorder {
             @Override
             public Object get() {
                 try {
+                    Executor executor = Infrastructure.getDefaultWorkerPool();
                     Class<? extends AbstractInProcessEmbeddingModel> loaded = (Class<? extends AbstractInProcessEmbeddingModel>) InProcessEmbeddingRecorder.class
                             .getClassLoader().loadClass(className);
-                    return loaded.getConstructor().newInstance();
+                    try {
+                        return loaded.getDeclaredConstructor(Executor.class).newInstance(executor);
+                    } catch (NoSuchMethodException e) {
+                        Logger.getLogger(InProcessEmbeddingRecorder.class)
+                                .warn("Cannot locate in-process embedding model's %s " +
+                                        "constructor that takes a java.util.concurrent.Executor, " +
+                                        "using the default constructor as fallback", className, e);
+                        return loaded.getDeclaredConstructor().newInstance();
+                    }
                 } catch (Exception e) {
                     Logger.getLogger(InProcessEmbeddingRecorder.class)
                             .errorf("Failed to instantiate in-process embedding model %s", className, e);
