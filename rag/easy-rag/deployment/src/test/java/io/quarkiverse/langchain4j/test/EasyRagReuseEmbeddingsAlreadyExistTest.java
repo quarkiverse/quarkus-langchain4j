38f1f44bd831ad495c61bdbaf8896d126eab508a
Clement Escoffier <clement.escoffier@gmail.com>
2024-10-02 16:53:00 +0200

    Add support for BGE 1.5 (regular and quantized) in process embedding
    - Add the BGE 1.5 local embedding
    - Update EasyRAG to exclude the dependency (it was a transitive dependency) but fix the dependency on the HuggingFace tokenizer
    - Add integration tests to verify the regular and quantized model in JVM mode and native
    - Add the new models to the in-process-embedding.adoc

--- rag/easy-rag/deployment/src/test/java/io/quarkiverse/langchain4j/test/EasyRagReuseEmbeddingsAlreadyExistTest.java
+++ rag/easy-rag/deployment/src/test/java/io/quarkiverse/langchain4j/test/EasyRagReuseEmbeddingsAlreadyExistTest.java
@@ -12,6 +12,7 @@ import jakarta.enterprise.inject.spi.CDI;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
@@ -20,6 +21,7 @@ import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.store.embedding.EmbeddingMatch;
 import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 import io.quarkus.test.QuarkusUnitTest;
 
 class EasyRagReuseEmbeddingsAlreadyExistTest {
@@ -62,5 +64,21 @@ class EasyRagReuseEmbeddingsAlreadyExistTest {
         question = embeddingModel.embed("When was David born?").content();
         matches = embeddingStore.findRelevant(question, 1);
         assertTrue(matches.get(0).embedded().text().contains("2003"));
+    }
+
+    @Test
+    @Disabled("Only when changing the embeddings model")
+    public void regenerateEmbeddingStoreOnFileSystem() {
+        EmbeddingModel embeddingModel = CDI.current().select(EmbeddingModel.class).get();
+        EmbeddingStore embeddingStore = CDI.current().select(EmbeddingStore.class).get();
+        TextSegment segment = TextSegment.textSegment("Charlie was born in 2005.");
+        embeddingStore.add(embeddingModel.embed(segment).content(), segment);
+
+        segment = TextSegment.textSegment("David was born in 2003.");
+        embeddingStore.add(embeddingModel.embed(segment).content(), segment);
+
+        assertThat(embeddingStore).isInstanceOf(InMemoryEmbeddingStore.class);
+
+        ((InMemoryEmbeddingStore) embeddingStore).serializeToFile("src/test/resources/embeddings/embeddings.json");
     }
 }
