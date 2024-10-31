b9199d7b8fc071f2b88df461fbdb45be3c6d1043
Jan Martiska <jmartisk@redhat.com>
2024-10-31 10:06:28 +0100

    Make the web-search example more reliable in answering questions related to today's date

--- samples/chatbot-web-search/src/main/java/io/quarkiverse/langchain4j/sample/chatbot/Bot.java
+++ samples/chatbot-web-search/src/main/java/io/quarkiverse/langchain4j/sample/chatbot/Bot.java
@@ -1,5 +1,6 @@
 package io.quarkiverse.langchain4j.sample.chatbot;
 
+import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.web.search.WebSearchTool;
 import io.quarkiverse.langchain4j.RegisterAiService;
@@ -9,6 +10,13 @@ import jakarta.enterprise.context.SessionScoped;
 @SessionScoped
 public interface Bot {
 
+    @SystemMessage("""
+            You have access to a function that looks up data using the Tavily web search engine.
+            The web search engine doesn't understand the concept of 'today',
+            so if the user asks something
+            that requires the knowledge of today's date, use the getTodaysDate function before
+            calling the search engine.
+            """)
     String chat(@UserMessage String question);
 
 }
