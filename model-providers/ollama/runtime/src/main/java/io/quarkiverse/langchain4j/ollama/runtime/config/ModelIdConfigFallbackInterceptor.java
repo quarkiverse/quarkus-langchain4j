89de624bf4d63b38375fc2a175e5f27a9d6b8758
Georgios Andrianakis <geoand@gmail.com>
2024-11-05 11:30:47 +0200

    Fix warning about missing model-id in Ollama
    Fixes: #1043

--- model-providers/ollama/runtime/src/main/java/io/quarkiverse/langchain4j/ollama/runtime/config/ModelIdConfigFallbackInterceptor.java
+++ model-providers/ollama/runtime/src/main/java/io/quarkiverse/langchain4j/ollama/runtime/config/ModelIdConfigFallbackInterceptor.java
@@ -1,11 +1,30 @@
 package io.quarkiverse.langchain4j.ollama.runtime.config;
 
+import java.util.function.Function;
+
 import io.smallrye.config.FallbackConfigSourceInterceptor;
 
 public class ModelIdConfigFallbackInterceptor extends FallbackConfigSourceInterceptor {
 
+    private static final Function<String, String> FALLBACK = new Function<>() {
+        @Override
+        public String apply(String name) {
+            if (!name.startsWith("quarkus.langchain4j.ollama")) {
+                return name;
+            }
+            if (!name.endsWith("model-id")) {
+                return name;
+            }
+            int index = name.lastIndexOf(".model-id");
+            if (index < 1) {
+                return name;
+            }
+            return name.substring(0, index) + ".model-name";
+        }
+    };
+
     public ModelIdConfigFallbackInterceptor() {
-        super(ModelIdToModelNameFunction.INSTANCE);
+        super(FALLBACK);
     }
 
 }
