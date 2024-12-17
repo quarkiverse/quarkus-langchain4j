651fc8cc314e310392059c74de811e7fd2e3646d
Georgios Andrianakis <geoand@gmail.com>
2024-12-17 10:10:14 +0200

    Bump samples to Quarkus 3.15.2

--- samples/jbang-joke-bot/jokes.java
+++ samples/jbang-joke-bot/jokes.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.quarkus.platform:quarkus-bom:3.15.1@pom
+//DEPS io.quarkus.platform:quarkus-bom:3.15.2@pom
 //DEPS io.quarkus:quarkus-picocli
 //DEPS io.quarkiverse.langchain4j:quarkus-langchain4j-openai:0.21.0.CR4
 //Q:CONFIG quarkus.banner.enabled=false
