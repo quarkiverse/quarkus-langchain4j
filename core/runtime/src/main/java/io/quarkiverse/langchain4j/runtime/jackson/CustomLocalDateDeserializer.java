69518808a61596f4bacb55c1bc852a8dea70cebc
Georgios Andrianakis <geoand@gmail.com>
2025-02-25 16:43:19 +0200

    Treat nonsense LocalDate (and friends) values as null
    This is done because LLMs can sometimes return
    erroneous values as dates, and thus it makes sense
    to just ignore them

--- core/runtime/src/main/java/io/quarkiverse/langchain4j/runtime/jackson/CustomLocalDateDeserializer.java
+++ core/runtime/src/main/java/io/quarkiverse/langchain4j/runtime/jackson/CustomLocalDateDeserializer.java
@@ -1,7 +1,10 @@
 package io.quarkiverse.langchain4j.runtime.jackson;
 
 import java.io.IOException;
+import java.time.DateTimeException;
 import java.time.LocalDate;
+
+import org.jboss.logging.Logger;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
@@ -14,6 +17,9 @@ import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
  * Often LLMs return a date as a JSON object containing the date's constituents
  */
 public class CustomLocalDateDeserializer extends JsonDeserializer<LocalDate> {
+
+    private static final Logger log = Logger.getLogger(CustomLocalDateDeserializer.class);
+
     @Override
     public LocalDate deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
         if (p.currentToken() == JsonToken.START_OBJECT) {
@@ -21,7 +27,13 @@ public class CustomLocalDateDeserializer extends JsonDeserializer<LocalDate> {
             int year = node.get("year").asInt();
             int month = node.get("month").asInt();
             int day = node.get("day").asInt();
-            return LocalDate.of(year, month, day);
+            try {
+                return LocalDate.of(year, month, day);
+            } catch (DateTimeException e) {
+                log.debug("Failed to deserialize LocalDate", e);
+                // in this case the LLM returned something that makes no sense (like all fields being zero), so best treat it as null
+                return null;
+            }
         } else {
             return LocalDateDeserializer.INSTANCE.deserialize(p, ctxt);
         }
--- core/runtime/src/main/java/io/quarkiverse/langchain4j/runtime/jackson/CustomLocalTimeDeserializer.java
+++ core/runtime/src/main/java/io/quarkiverse/langchain4j/runtime/jackson/CustomLocalTimeDeserializer.java
@@ -3,6 +3,8 @@ package io.quarkiverse.langchain4j.runtime.jackson;
 import java.io.IOException;
 import java.time.LocalTime;
 
+import org.jboss.logging.Logger;
+
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
@@ -14,6 +16,9 @@ import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
  * Often LLMs return a time as a JSON object containing the time's constituents
  */
 public class CustomLocalTimeDeserializer extends JsonDeserializer<LocalTime> {
+
+    private static final Logger log = Logger.getLogger(CustomLocalTimeDeserializer.class);
+
     @Override
     public LocalTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
         if (p.currentToken() == JsonToken.START_OBJECT) {
@@ -22,7 +27,13 @@ public class CustomLocalTimeDeserializer extends JsonDeserializer<LocalTime> {
             int minute = node.get("minute").asInt();
             int second = node.get("second").asInt();
             int nano = node.get("nano").asInt();
-            return LocalTime.of(hour, minute, second, nano);
+            try {
+                return LocalTime.of(hour, minute, second, nano);
+            } catch (Exception e) {
+                log.debug("Failed to deserialize LocalTime", e);
+                // in this case the LLM returned something that makes no sense (like all fields being zero), so best treat it as null
+                return null;
+            }
         } else {
             return LocalTimeDeserializer.INSTANCE.deserialize(p, ctxt);
         }
