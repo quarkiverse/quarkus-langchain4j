364e48d1075a09a17be3df283fc65627800ae5fe
Alex Soto <asotobu@gmail.com>
2025-02-26 16:48:09 +0100

    Adds Javadoc for AI Gemini model

--- model-providers/google/gemini/ai-gemini/runtime/src/main/java/io/quarkiverse/langchain4j/ai/runtime/gemini/EmbedContentResponse.java
+++ model-providers/google/gemini/ai-gemini/runtime/src/main/java/io/quarkiverse/langchain4j/ai/runtime/gemini/EmbedContentResponse.java
@@ -1,5 +1,12 @@
 package io.quarkiverse.langchain4j.ai.runtime.gemini;
 
+/**
+ * The response body contains data.
+ *
+ * @param embedding The embedding generated from the input content.
+ *
+ * @see <a href="https://ai.google.dev/api/embeddings#response-body">EmbedContentResponse</a>
+ */
 public record EmbedContentResponse(Embedding embedding) {
     public record Embedding(float[] values) {
     }
--- model-providers/google/gemini/ai-gemini/runtime/src/main/java/io/quarkiverse/langchain4j/ai/runtime/gemini/ErrorResponse.java
+++ model-providers/google/gemini/ai-gemini/runtime/src/main/java/io/quarkiverse/langchain4j/ai/runtime/gemini/ErrorResponse.java
@@ -1,5 +1,9 @@
 package io.quarkiverse.langchain4j.ai.runtime.gemini;
 
+/**
+ *
+ * Error information.
+ */
 public record ErrorResponse(ErrorInfo error) {
     public record ErrorInfo(Integer code, String message, String status) {
     }
