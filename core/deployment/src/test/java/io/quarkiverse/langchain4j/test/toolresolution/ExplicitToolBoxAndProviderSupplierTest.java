ccc7c6f364c0e07324e66d2daf89ba22956bf9f7
mariofusco <mario.fusco@gmail.com>
2025-04-07 18:09:06 +0200

    add test

--- core/deployment/src/test/java/io/quarkiverse/langchain4j/test/toolresolution/ExplicitToolBoxAndProviderSupplierTest.java
+++ core/deployment/src/test/java/io/quarkiverse/langchain4j/test/toolresolution/ExplicitToolBoxAndProviderSupplierTest.java
@@ -1,25 +1,23 @@
 package io.quarkiverse.langchain4j.test.toolresolution;
 
+import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.UserMessage;
+import io.quarkiverse.langchain4j.RegisterAiService;
+import io.quarkiverse.langchain4j.ToolBox;
+import io.quarkus.test.QuarkusUnitTest;
 import jakarta.enterprise.context.control.ActivateRequestContext;
 import jakarta.inject.Inject;
-
-import org.assertj.core.api.Assertions;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
-
-import dev.langchain4j.service.MemoryId;
-import dev.langchain4j.service.UserMessage;
-import io.quarkiverse.langchain4j.RegisterAiService;
-import io.quarkus.test.QuarkusUnitTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  *
  */
-public class ExplicitToolsAndProviderSupplierTest {
+public class ExplicitToolBoxAndProviderSupplierTest {
 
     @RegisterExtension
     static final QuarkusUnitTest unitTest = new QuarkusUnitTest()
@@ -31,9 +29,10 @@ public class ExplicitToolsAndProviderSupplierTest {
                             MyCustomToolProvider.class,
                             ToolsClass.class));
 
-    @RegisterAiService(toolProviderSupplier = MyCustomToolProviderSupplier.class, tools = ToolsClass.class, chatLanguageModelSupplier = TestAiSupplier.class)
+    @RegisterAiService(toolProviderSupplier = MyCustomToolProviderSupplier.class, chatLanguageModelSupplier = TestAiSupplier.class)
     interface ServiceWithToolClash {
 
+        @ToolBox(ToolsClass.class)
         String chat(@UserMessage String msg, @MemoryId Object id);
 
     }
