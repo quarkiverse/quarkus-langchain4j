5d0c44a95e27dbea6b399056a9e7189eee78621f
Sergey Beryozkin <sberyozkin@gmail.com>
2025-05-06 15:59:12 +0100

    Add McpAuthProvider

--- mcp/deployment/src/main/java/io/quarkiverse/langchain4j/mcp/deployment/McpProcessor.java
+++ mcp/deployment/src/main/java/io/quarkiverse/langchain4j/mcp/deployment/McpProcessor.java
@@ -23,6 +23,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import dev.langchain4j.mcp.client.McpClient;
 import dev.langchain4j.service.tool.ToolProvider;
+import io.quarkiverse.langchain4j.mcp.auth.McpAuthProvider;
 import io.quarkiverse.langchain4j.mcp.runtime.McpClientName;
 import io.quarkiverse.langchain4j.mcp.runtime.McpRecorder;
 import io.quarkiverse.langchain4j.mcp.runtime.config.LocalLaunchParams;
@@ -30,6 +31,7 @@ import io.quarkiverse.langchain4j.mcp.runtime.config.McpBuildTimeConfiguration;
 import io.quarkiverse.langchain4j.mcp.runtime.config.McpRuntimeConfiguration;
 import io.quarkiverse.langchain4j.mcp.runtime.config.McpTransportType;
 import io.quarkus.arc.deployment.SyntheticBeanBuildItem;
+import io.quarkus.arc.deployment.UnremovableBeanBuildItem;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
@@ -183,6 +185,11 @@ public class McpProcessor {
                         .produce(ReflectiveClassBuildItem.builder(clazz.name().toString()).fields(true).methods(true).build());
             }
         }
+    }
+
+    @BuildStep
+    public void addMcpAuthProvider(BuildProducer<UnremovableBeanBuildItem> unremovableProducer) {
+        unremovableProducer.produce(UnremovableBeanBuildItem.beanTypes(McpAuthProvider.class));
     }
 
 }
