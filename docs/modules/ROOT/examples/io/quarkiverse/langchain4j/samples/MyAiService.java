db62afb0224b5e339aed2ca08d664b084abaf341
Clement Escoffier <clement.escoffier@gmail.com>
2025-06-11 18:03:09 +0200

    Revamp documentation.

--- docs/modules/ROOT/examples/io/quarkiverse/langchain4j/samples/MyAiService.java
+++ docs/modules/ROOT/examples/io/quarkiverse/langchain4j/samples/MyAiService.java
@@ -1,17 +1,19 @@
 package io.quarkiverse.langchain4j.samples;
 
+import jakarta.enterprise.context.ApplicationScoped;
+
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
 
-@RegisterAiService( // <1>
-        tools = EmailService.class // <2>
-)
+@RegisterAiService // <1>
+@SystemMessage("You are a professional poet") // <2>
+@ApplicationScoped // <3>
 public interface MyAiService {
 
-    @SystemMessage("You are a professional poet") // <3>
     @UserMessage("""
-                Write a poem about {topic}. The poem should be {lines} lines long. Then send this poem by email. // <4>
+                Write a poem about {topic}.
+                The poem should be {lines} lines long. <4>
             """)
     String writeAPoem(String topic, int lines); // <5>
 }
