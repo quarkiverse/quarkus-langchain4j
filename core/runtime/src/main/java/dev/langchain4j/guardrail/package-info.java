7772809ff746b7cfb81c95078de1d3c36a7631f3
Eric Deandrea <eric.deandrea@gmail.com>
2025-08-13 11:39:57 -0400

    Finishing guardrails implementation

--- core/runtime/src/main/java/dev/langchain4j/guardrail/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This is here just for the short term until https://github.com/langchain4j/langchain4j/pull/3470 is released and merged into
- * Quarkus LangChain4j
- */
-package dev.langchain4j.guardrail;
