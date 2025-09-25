16947cd126114ff1c32f29b9a2cb741fc7587523
Fedor Dudinsky <fdudinsk@redhat.com>
2025-09-25 16:07:44 +0200

    Add tests for movie-search sample
    - Change the pom to allow depending on the platform BOM
    - Add tests to the module
    - Add the module into parent `samples` module
    
    Cherry-pick commit d9510859415e539d8285fac2490ecc02971521b7

---
+++ samples/movie-similarity-search/src/test/java/io/quarkiverse/langchain4j/sample/tests/MovieIT.java
@@ -0,0 +1,10 @@
+package io.quarkiverse.langchain4j.sample.tests;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Disabled;
+
+@QuarkusIntegrationTest
+@Disabled("https://github.com/quarkiverse/quarkus-langchain4j/issues/1814")
+public class MovieIT extends MovieTest {
+
+}
