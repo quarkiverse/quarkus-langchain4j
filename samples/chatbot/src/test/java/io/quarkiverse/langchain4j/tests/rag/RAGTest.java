2a15a786c8c3d4d2bf9dd6eeaf0078a91b3915a9
Fedor Dudinsky <fdudinsk@redhat.com>
2025-12-03 14:29:24 +0100

    Add verfication for gen_ai metrics in tests
    and do some refacotring in RAG sample

--- samples/chatbot/src/test/java/io/quarkiverse/langchain4j/tests/rag/RAGTest.java
+++ samples/chatbot/src/test/java/io/quarkiverse/langchain4j/tests/rag/RAGTest.java
@@ -9,17 +9,25 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.net.http.WebSocket;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @QuarkusTest
 public class RAGTest {
@@ -27,6 +35,7 @@ public class RAGTest {
 
     private List<String> answers;
     private WebSocket webSocket;
+    private HttpClient client;
 
     @TestHTTPResource
     URL url;
@@ -40,12 +49,13 @@ public class RAGTest {
                 socket, null, null);
         LOG.info("Connecting to: " + socket + " at " + uri);
         answers = Collections.synchronizedList(new ArrayList<>());
-        webSocket = HttpClient.newHttpClient().newWebSocketBuilder()
+        client = HttpClient.newHttpClient();
+        webSocket = client.newWebSocketBuilder()
                 .buildAsync(uri, new WebSocketListener(answers)).get();
     }
 
     @AfterEach
-    void tearDown() throws ExecutionException, InterruptedException {
+    void tearDown() throws Exception {
         answers = null;
         webSocket.sendClose(WebSocket.NORMAL_CLOSURE, "").get();
     }
@@ -59,7 +69,7 @@ public class RAGTest {
     }
 
     @Test
-    public void documentBasedAnswer() throws InterruptedException {
+    public void documentBasedAnswer() throws InterruptedException, URISyntaxException, IOException {
         // Input tokens are usually much cheaper, than output tokens, so let's stop the LLM from talking too much
         String prompt = "What is the opening deposit (in USD) for a standard savings account? Answer with number only";
         webSocket.sendText(prompt, true);
@@ -68,18 +78,55 @@ public class RAGTest {
         // We need to wait for the end of the answer, detected as no new messages during a second.
         // The prompt should protect against this, but it is not guaranteed.
         int repeats = 0;
-        int lastSize=0;
-        while (answers.size() <= 1 || answers.size()!=lastSize) {
+        int lastSize = 0;
+        while (answers.size() <= 1 || answers.size() != lastSize) {
             if (repeats++ > 10) {
                 LOG.warn("We have waited for: " + repeats + " seconds and it is too much!");
                 break;
             }
-            lastSize=answers.size();
+            lastSize = answers.size();
             Thread.sleep(1000);
         }
 
         String response = String.join("", answers);
-        Assertions.assertTrue(response.contains("25"));
+        assertTrue(response.contains("25"));
+
+        // Now, lets verify, that ai metrics were created:
+        URI uri = new URI("http", null,
+                url.getHost(),
+                url.getPort(),
+                "/q/metrics", null, null);
+        HttpResponse<String> metricsResponse = client.send(HttpRequest.newBuilder().GET().uri(uri).build(),
+                HttpResponse.BodyHandlers.ofString());
+        Assertions.assertEquals(200, metricsResponse.statusCode());
+        LinesArray metrics = new LinesArray(Stream.of(metricsResponse.body().split("\n"))
+                .filter(line -> line.startsWith("gen_ai"))
+                .toList());
+        assertTrue(metrics.containsLine("gen_ai_client_estimated_cost_total"));
+        assertTrue(metrics.containsLine("gen_ai_client_operation_duration_seconds_max"));
+        assertTrue(metrics.containsLine("gen_ai_client_operation_duration_seconds_count"));
+        assertTrue(metrics.containsLine("gen_ai_client_operation_duration_seconds_sum"));
+        assertTrue(metrics
+                .containsLine(line -> {
+                    boolean first = line.startsWith("gen_ai_client_token_usage_total");
+                    boolean second = line.contains("""
+                            gen_ai_token_type="input"
+                            """.trim());
+                    return first && second;
+                }),"There is no metric for input tokens!");
+        String outputTokensMetric = metrics.getLine(line -> {
+                    boolean first = line.startsWith("gen_ai_client_token_usage_total");
+                    boolean second = line.contains("""
+                            gen_ai_token_type="output"
+                            """.trim());
+                    return first && second;
+                })
+                .orElseThrow(() -> new AssertionError("There is no metric for output tokens!"));
+        assertTrue(outputTokensMetric.contains("ai_service_class_name=\"io.quarkiverse.langchain4j.sample.chatbot.Bot\""));
+        assertTrue(outputTokensMetric.contains("gen_ai_request_model=\"gpt-4o-mini\""));
+        String tokens = outputTokensMetric.split("} ")[1];
+        Assertions.assertEquals("1.0", tokens, "Wrong number of tokens!");
+
     }
 
     class WebSocketListener implements WebSocket.Listener {
@@ -108,5 +155,24 @@ public class RAGTest {
             }
             return WebSocket.Listener.super.onText(webSocket, data, last);
         }
+    }
+
+    record LinesArray(List<String> lines) {
+        LinesArray(List<String> lines) {
+            this.lines = lines;
+        }
+
+        boolean containsLine(String prefix) {
+            return this.getLine(line -> line.startsWith(prefix)).isPresent();
+        }
+
+        boolean containsLine(Predicate<? super String> condition) {
+            return this.getLine(condition).isPresent();
+        }
+
+        Optional<String> getLine(Predicate<? super String> condition) {
+            return lines.stream().filter(condition).findAny();
+        }
+
     }
 }
