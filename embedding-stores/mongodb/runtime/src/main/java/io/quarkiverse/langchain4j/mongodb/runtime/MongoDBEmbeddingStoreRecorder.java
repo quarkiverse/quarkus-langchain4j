4c9bd5b49786124e47023e528b9408643c94e38c
moheyeldin.badr <MoheyElDin.Badr@gmail.com>
2026-02-24 21:34:02 +0200

    Add score field support and enhance vector handling in MongoDB embedding store

--- embedding-stores/mongodb/runtime/src/main/java/io/quarkiverse/langchain4j/mongodb/runtime/MongoDBEmbeddingStoreRecorder.java
+++ embedding-stores/mongodb/runtime/src/main/java/io/quarkiverse/langchain4j/mongodb/runtime/MongoDBEmbeddingStoreRecorder.java
@@ -43,6 +43,7 @@ public class MongoDBEmbeddingStoreRecorder {
                         config.textFieldName(),
                         config.dimensions(),
                         config.similaritySearch(),
+                        config.scoreFieldName(),
                         config.metadataFieldName());
             }
         };
