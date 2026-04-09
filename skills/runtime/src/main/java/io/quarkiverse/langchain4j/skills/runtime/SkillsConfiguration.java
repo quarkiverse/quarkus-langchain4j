1e9ea3a98c5a2d589aab82603d9a628056aeadc4
Jan Martiska <wraychus@gmail.com>
2026-04-09 15:53:24 +0200

    Rebuild docs

--- skills/runtime/src/main/java/io/quarkiverse/langchain4j/skills/runtime/SkillsConfiguration.java
+++ skills/runtime/src/main/java/io/quarkiverse/langchain4j/skills/runtime/SkillsConfiguration.java
@@ -13,7 +13,8 @@ public interface SkillsConfiguration {
 
     /**
      * List of directories from which skills should be loaded.
-     * Each entry is either an absolute or relative path in the filesystem, or, if it's prepended with "classpath:", is considered a classpath resource.
+     * Each entry is either an absolute or relative path in the filesystem, or, if it's prepended with "classpath:", is
+     * considered a classpath resource.
      * A relative path is resolved against the current working directory at runtime.
      */
     Optional<List<String>> directories();
