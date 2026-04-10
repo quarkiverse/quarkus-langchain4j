99a2cfed1bca6a0070ef8c664f553d6bb6285d1c
mariofusco <mario.fusco@gmail.com>
2026-04-10 08:43:06 +0200

    fix

--- core/runtime/src/main/java/io/quarkiverse/langchain4j/runtime/jackson/ToolExecutionResultMessageMixin.java
+++ core/runtime/src/main/java/io/quarkiverse/langchain4j/runtime/jackson/ToolExecutionResultMessageMixin.java
@@ -1,10 +1,9 @@
 package io.quarkiverse.langchain4j.runtime.jackson;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import dev.langchain4j.data.message.ToolExecutionResultMessage;
 import io.quarkus.jackson.JacksonMixin;
@@ -12,12 +11,8 @@ import io.quarkus.jackson.JacksonMixin;
 @JacksonMixin(ToolExecutionResultMessage.class)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({ "text", "id", "toolName", "type" }) // needed for tests
-public class ToolExecutionResultMessageMixin {
+@JsonDeserialize(builder = ToolExecutionResultMessage.Builder.class)
+@JsonPropertyOrder({ "id", "toolName", "type", "contents", "attributes" })
+public abstract class ToolExecutionResultMessageMixin {
 
-    @JsonCreator
-    public ToolExecutionResultMessageMixin(@JsonProperty("id") String id, @JsonProperty("toolName") String toolName,
-            @JsonProperty("text") String text) {
-
-    }
 }
