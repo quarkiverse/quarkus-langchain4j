894181d5bc782e366ca37c9fa655a207c54a467c
Sergey Beryozkin <sberyozkin@gmail.com>
2026-06-25 17:27:31 +0100

    Add workload-model-auth-provider extension

---
+++ model-auth-providers/workload-model-auth-provider/deployment/src/main/java/io/quarkiverse/langchain4j/workload/deployment/WorkloadModelAuthProviderProcessor.java
@@ -0,0 +1,67 @@
+package io.quarkiverse.langchain4j.workload.deployment;
+
+import java.util.function.BooleanSupplier;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+import org.jboss.jandex.ClassType;
+import org.jboss.jandex.DotName;
+
+import io.quarkiverse.langchain4j.auth.ModelAuthProvider;
+import io.quarkiverse.langchain4j.workload.runtime.WorkloadModelAuthProviderRecorder;
+import io.quarkus.arc.deployment.SyntheticBeanBuildItem;
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.BuildSteps;
+import io.quarkus.deployment.annotations.ExecutionTime;
+import io.quarkus.deployment.annotations.Record;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.oidc.client.OidcClient;
+import io.quarkus.oidc.client.OidcClients;
+import io.vertx.core.Vertx;
+
+@BuildSteps(onlyIf = WorkloadModelAuthProviderProcessor.IsEnabled.class)
+public class WorkloadModelAuthProviderProcessor {
+
+    private static final String FEATURE = "langchain4j-workload-model-auth-provider";
+    private static final DotName MODEL_AUTH_PROVIDER = DotName.createSimple(ModelAuthProvider.class);
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    @Record(ExecutionTime.RUNTIME_INIT)
+    public void registerBeans(
+            WorkloadModelAuthProviderBuildConfig buildConfig,
+            BuildProducer<SyntheticBeanBuildItem> beanProducer,
+            WorkloadModelAuthProviderRecorder recorder) {
+
+        var builder = SyntheticBeanBuildItem
+                .configure(MODEL_AUTH_PROVIDER)
+                .setRuntimeInit()
+                .defaultBean()
+                .unremovable()
+                .scope(ApplicationScoped.class)
+                .addInjectionPoint(ClassType.create(DotName.createSimple(Vertx.class)));
+
+        if (buildConfig.oidcClientName().isPresent()) {
+            builder.addInjectionPoint(ClassType.create(DotName.createSimple(OidcClients.class)))
+                    .createWith(recorder.namedProvider(buildConfig.oidcClientName().get()));
+        } else {
+            builder.addInjectionPoint(ClassType.create(DotName.createSimple(OidcClient.class)))
+                    .createWith(recorder.defaultProvider());
+        }
+
+        beanProducer.produce(builder.done());
+    }
+
+    public static class IsEnabled implements BooleanSupplier {
+        WorkloadModelAuthProviderBuildConfig config;
+
+        public boolean getAsBoolean() {
+            return config.enabled().orElse(true);
+        }
+    }
+}
