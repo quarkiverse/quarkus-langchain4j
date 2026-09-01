bc6e01fa026ac09a18f939cea5769432bd823cd0
Jan Martiska <wraychus@gmail.com>
2026-09-01 09:58:17 +0200

    Update to Quarkus 3.33.3.1 and testing quarkus-mcp-server to 2.0.0

--- integration-tests/mcp/src/test/resources/tools_legacy_mcp_server.java
+++ integration-tests/mcp/src/test/resources/tools_legacy_mcp_server.java
@@ -1,7 +1,9 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.quarkus:quarkus-bom:${quarkus.version:3.33.2.1}@pom
+//DEPS io.quarkus:quarkus-bom:${quarkus.version:3.33.3.1}@pom
 //DEPS io.quarkiverse.mcp:quarkus-mcp-server-stdio:1.13.1
 //DEPS io.quarkiverse.mcp:quarkus-mcp-server-http:1.13.1
+// NOTE: do NOT update quarkus-mcp-server to 2.x in this file
+// we want to be sure that the server only supports the legacy stateful protocol
 
 import io.quarkiverse.mcp.server.Tool;
 import io.quarkiverse.mcp.server.ToolArg;
